1cc4da36fd8b45a45d3a4701f5502492cea00896
ImKunYoung <ur2kunyoung2@outlook.com>
2023-09-19 13:39:34 +0900

    39. JavaScript Logic in Components

--- src/index.js
+++ src/index.js
@@ -64,6 +64,16 @@ function Menu() {
 }
 
 function Footer() {
+    const hour = new Date().getHours();
+    const openHour = 8;
+    const closeHour = 22;
+    const isOpen = openHour <= hour <= closeHour;
+    console.log(isOpen);
+
+
+    // if (openHour <= hour <= closeHour) alert("We are currently open!");
+    // else alert("Sorry We are currently closed!");
+
     return <footer>{new Date().toLocaleTimeString()} We are currently open!</footer>;
     // return React.createElement('footer', null, '© We are currently open!');
 }
